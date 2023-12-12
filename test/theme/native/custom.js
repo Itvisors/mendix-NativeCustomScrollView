ecaf96a9a57ddd2d8b66d60542e5a14bd40523bf
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2023-12-12 13:27:32 +0100

    Mendix 9.24, widget tools 9.17

---
+++ test/theme/native/custom.js
@@ -0,0 +1,9 @@
+import { brand } from "./custom-variables";
+import { Dimensions } from "react-native";
+
+const { height, width } = Dimensions.get("window");
+export const containerHorizontalScrollView = {
+  container: {
+    width: width
+  }
+};
